d0e12b7b59c41ec7b4f6e63c4df1d65c99e5d8f9
mxnan <128995981+mxnan@users.noreply.github.com>
2024-04-29 23:53:21 +0530

    minor
    destructured theme button ,all in container folder

--- src/components/container/navbar.tsx
+++ src/components/container/navbar.tsx
@@ -1,53 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
-
 import Navlinks from "./navlinks";
+import Themebutton from "./theme-toggle";
 
 const Navbar = () => {
-  // State for handling theme (dark/light mode)
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
-
-  // After mounting, set mounted to true to enable theme handling
-  useEffect(() => setMounted(true), []);
-
   return (
     <>
       {/* Navigation bar */}
       <header className="sticky-nav font-title flex justify-between items-center max-w-4xl w-full p-8 mx-auto bg-opacity-60">
         {/* Dark mode toggle button */}
-        <button
-          aria-label="Toggle Dark Mode"
-          type="button"
-          className="dark-mode-toggle"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          {mounted && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              stroke="currentColor"
-              className="h-5 w-5 text-violet-600 dark:text-amber-600"
-            >
-              {theme === "dark" ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              )}
-            </svg>
-          )}
-        </button>
+        <Themebutton />
         <Navlinks />
       </header>
     </>
